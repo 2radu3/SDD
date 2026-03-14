bc5d2ae932746ba4e953c8cbbc14ddf83da49bf2
2radu3 <2radupmihai3@gmail.com>
2026-03-14 23:21:31 +0200

    I have questions!!

---
+++ practice/problem_07.c
@@ -0,0 +1,92 @@
+//Deletion from a Circular Linked List
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node Node;
+struct Node {
+    int data;
+    Node* next;
+};
+
+void addToList(Node** head, int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    if((*head == NULL)) {
+        *head = newNode;
+        newNode->next = *head;
+    } else {
+        Node* aux = *head;
+        while(aux->next != *head) {
+            aux = aux->next;
+        }
+        aux->next = newNode;
+        newNode->next = *head;
+    }
+}
+
+void print(Node* head) {
+    Node* temp = head;
+    printf("\nData: %d", head->data);
+    head = head->next;
+    while(head != temp) {
+        printf("\nData: %d", head->data);
+        head = head->next;
+    }
+}
+
+void deleteFirstNode(Node** head) { //must ask which one its better
+    if((*head) == NULL) return;
+
+    Node* temp = *head;
+    while(temp->next != *head) {
+        temp = temp->next;
+    }
+
+    if((*head) == temp) {
+        free(*head);
+        *head = NULL;
+    } else {
+        Node* aux = *head;
+        temp->next = (*head)->next;
+        *head = (*head)->next;
+        free(aux);
+    }    
+}
+
+Node* deleteFirstNode2(struct Node* last) {
+    if (last == NULL) return NULL;
+
+    struct Node* head = last->next;
+
+    if (head == last) {
+        free(head);
+        last = NULL;
+    } else {
+        last->next = head->next;
+        free(head);
+    }
+
+    return last;
+}
+
+int main() {
+    Node* head;
+    addToList(&head, 1);
+    addToList(&head, 2);
+    addToList(&head, 3);
+    addToList(&head, 4);
+    //deleteFirstNode(&head);
+    //print(head);
+
+    Node* last = head;
+
+    while(last->next != head) {
+        last = last->next;
+    }
+
+    last = deleteFirstNode2(last);
+    print(last);
+
+    return 0;
+}
