64b2c8621da1cf86d03e0782670a7a026273205e
Putureanu Radu Mihai <2radupmihai3@gmail.com>
2026-03-17 21:32:42 +0200

    implemented deletion of the last and middle nodes

--- practice/problem_07.c
+++ practice/problem_07.c
@@ -11,13 +11,17 @@ struct Node {
 
 void addToList(Node** head, int data) {
     Node* newNode = (Node*)malloc(sizeof(Node));
+    if (newNode == NULL) {
+        return;
+    }
     newNode->data = data;
-    if((*head == NULL)) {
+    if ((*head == NULL)) {
         *head = newNode;
         newNode->next = *head;
-    } else {
+    }
+    else {
         Node* aux = *head;
-        while(aux->next != *head) {
+        while (aux->next != *head) {
             aux = aux->next;
         }
         aux->next = newNode;
@@ -29,29 +33,30 @@ void print(Node* head) {
     Node* temp = head;
     printf("\nData: %d", head->data);
     head = head->next;
-    while(head != temp) {
+    while (head != temp) {
         printf("\nData: %d", head->data);
         head = head->next;
     }
 }
 
 void deleteFirstNode(Node** head) { //must ask which one its better
-    if((*head) == NULL) return;
+    if ((*head) == NULL) return;    // --both works
 
     Node* temp = *head;
-    while(temp->next != *head) {
+    while (temp->next != *head) {
         temp = temp->next;
     }
 
-    if((*head) == temp) {
+    if ((*head) == temp) {
         free(*head);
         *head = NULL;
-    } else {
+    }
+    else {
         Node* aux = *head;
         temp->next = (*head)->next;
         *head = (*head)->next;
         free(aux);
-    }    
+    }
 }
 
 Node* deleteFirstNode2(struct Node* last) {
@@ -62,31 +67,72 @@ Node* deleteFirstNode2(struct Node* last) {
     if (head == last) {
         free(head);
         last = NULL;
-    } else {
+    }
+    else {
         last->next = head->next;
         free(head);
     }
-
     return last;
 }
 
+void deleteSpecificNode(Node** head, int key) {
+    //check if the list is empty
+    if ((*head) == NULL) return;
+
+    //if the first node matches the key
+    Node* curr = *head;
+    if (curr->data == key) {
+        //if it is the only node in the list
+        if (curr->next == *head) {
+            free(curr);
+            *head = NULL;
+            return;
+        }
+
+        //if it has more nodes we must find the last node
+        Node* last = *head;
+        while (last->next != *head) {
+            last = last->next;
+        }
+        last->next = curr->next;
+        *head = curr->next;
+        free(curr);
+        return;
+    }
+
+    //if the node is last or in the middle
+    Node* prev = *head;
+    curr = (*head)->next;
+    while (curr != *head) {
+        if (curr->data == key) {
+            prev->next = curr->next;
+            free(curr);
+            return;
+        }
+        prev = curr;
+        curr = curr->next;
+    }
+}
+
+
 int main() {
-    Node* head;
+    Node* head = NULL;
     addToList(&head, 1);
     addToList(&head, 2);
     addToList(&head, 3);
     addToList(&head, 4);
-    //deleteFirstNode(&head);
+    deleteFirstNode(&head);
     //print(head);
 
-    Node* last = head;
+    //Node* last = head;
+    //while (last->next != head) {
+        //last = last->next;
+    //}
+    //last = deleteFirstNode2(last);
+    //print(last);
 
-    while(last->next != head) {
-        last = last->next;
-    }
-
-    last = deleteFirstNode2(last);
-    print(last);
+    deleteSpecificNode(&head, 2);
+    print(head);
 
     return 0;
 }
